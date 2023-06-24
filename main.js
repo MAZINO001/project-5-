74d586dc291137bcb0ea47759c14d337940c70e5
mounir <marnissimounir05@gmail.com>
2023-06-24 19:44:44 +0100

    update one

--- main.js
+++ main.js
@@ -12,4 +12,23 @@ function drop(event) {
   event.target.appendChild(document.getElementById(data));
 }
 
+function resetGame() {
+  // Clear the content of each dropbox on the board
+  const dropboxes = document.getElementsByClassName("dropbox");
+  for (let i = 0; i < dropboxes.length; i++) {
+    dropboxes[i].innerHTML = "";
+  }
 
+  // Move the cross and circle elements back to the drag container
+  const dragContainer = document.querySelector(".drag");
+  const dragElements = document.querySelectorAll(".dragbox");
+  dragElements.forEach((element) => {
+    dragContainer.appendChild(element);
+  });
+}
+
+// Get the reset button element
+const resetButton = document.querySelector(".reset");
+
+// Attach the click event listener to the reset button
+resetButton.addEventListener("click", resetGame);
